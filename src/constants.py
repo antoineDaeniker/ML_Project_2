19b5001f09db70ef209185f811ed4d9567f387c1
Tim Poštuvan <tim.postuvan@gmail.com>
2021-12-15 00:56:44 +0100

    Update constants

--- src/constants.py
+++ src/constants.py
@@ -1,13 +1,10 @@
-import torch
-import os
-
 # define the crop image dimensions before resizing
-CROP_IMAGE_WIDTH = 16 # maybe 8
-CROP_IMAGE_HEIGHT = 16 # maybe 8
+CROP_IMAGE_WIDTH = 16
+CROP_IMAGE_HEIGHT = 16
 
 # define the input image dimensions
-INPUT_IMAGE_WIDTH = 32 # maybe 16
-INPUT_IMAGE_HEIGHT = 32 # maybe 16
+INPUT_IMAGE_WIDTH = 16
+INPUT_IMAGE_HEIGHT = 16
 
 # full image dimension
 FULL_IMAGE_WIDTH = 2048
